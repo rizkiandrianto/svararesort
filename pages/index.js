a21a12c8bdf684dc3200c848ae44e8e7655ff718
Rizki Andrianto <rizki_andrianto@rocketmail.com>
2021-03-18 22:11:02 +0700

    :wrench: Change home section to env

--- pages/index.js
+++ pages/index.js
@@ -20,9 +20,9 @@ const Home = ({ video }) => {
   const [gallery, setGallery] = useState([]);
   const [loading, setLoading] = useState(true);
   useEffect(async () => {
-    await fetch('/posts?categories=4').then((secs) => setSections(secs.reverse()));
+    await fetch(`/posts?categories=${process.env.NEXT_PUBLIC_HOMESECTION_ID}`).then((secs) => setSections(secs.reverse()));
     if (typeof window !== 'undefined') document.querySelector('body').classList.add('finish');
-    await fetch('/posts/50').then((post) => setGallery((post.gallery[0] || []).reverse()));
+    await fetch(`/posts/${process.env.NEXT_PUBLIC_HOMESLIDER_ID}`).then((post) => setGallery((post.gallery[0] || []).reverse()));
 
     setLoading(false);
     AOS.init();
