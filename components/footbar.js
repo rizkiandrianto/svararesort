384b936e01042984b7635f68efe8a3e1e25b5c3a
Rizki Andrianto <rizki_andrianto@rocketmail.com>
2021-03-19 19:00:37 +0700

    :fire: Fix error no socmed menu

--- components/footbar.js
+++ components/footbar.js
@@ -5,8 +5,8 @@ const Footbar = () => {
   const [menus, setMenus] = useState([]);
   const [scrolled, setScrolled] = useState(false);
   useEffect(async () => {
-    await fetch(`/wp-api-menus/v2/menus/${process.env.NEXT_PUBLIC_SOCMED_ID}`, { baseUrl: `${process.env.NEXT_PUBLIC_API_HOST}/wp-json` })
-      .then((menu) => setMenus(menu.items))
+    await fetch(`/wp-api-menus/v2/menus/''`, { baseUrl: `${process.env.NEXT_PUBLIC_API_HOST}/wp-json` })
+      .then((menu) => setMenus(menu.items || []))
 
     function updateScrollPosition() {
       const offset = 56;
