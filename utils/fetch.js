6c8025b547f15ab9f5c85db5feec0076e3f62c7c
Rizki Andrianto <rizki_andrianto@rocketmail.com>
2021-03-21 21:04:29 +0700

    :sparkles: Ability to set Headers on Request

--- utils/fetch.js
+++ utils/fetch.js
@@ -1,10 +1,26 @@
 const Fetch = (url, param) => {
   const basic = { baseUrl: `${process.env.NEXT_PUBLIC_API_HOST}/wp-json/wp/v2` };
   const settings = param ? { ...basic, ...param } : basic;
-  const { baseUrl } = settings;
+  const { baseUrl, headers, ...setting } = settings;
   const finalUrl = baseUrl ? baseUrl + url : url;
+  const options = {
+    ...(setting || {}),
+    redirect: 'follow'
+  }
 
-  return fetch(finalUrl).then((res => res.json()));
+  if (headers) {
+    const newHeader = {};
+    Object.keys(headers).forEach((header) => {
+      newHeader[header] = headers[header];
+    });
+    options.headers = newHeader;
+  }
+
+  return fetch(finalUrl, options).then((res => {
+    if (res.status === 200) return res.json();
+
+    return false;
+  }));
 };
 
 module.exports = {
